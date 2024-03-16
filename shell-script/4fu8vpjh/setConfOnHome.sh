f3a0f5a90d47d1d05541d4f3615b86c590db4070
IDShogun <me.creampuffs@gmail.com>
2024-03-16 17:01:07 +0900

    Add shell-script/4fu8vpjh/setConfOnHome

---
+++ shell-script/4fu8vpjh/setConfOnHome.sh
@@ -0,0 +1,8 @@
+rm -rf ~/bin/
+rm -rf ~/.config/nvim/
+rm -rf ~/envconf/
+
+git clone https://github.com/id-shogun/envconf.git
+
+cp -r ~/envconf/nvim/sp8xtxb8/ ~/.config/nvim/
+cp -r ~/envconf/shell-script/4fu8vpjh/ ~/bin/
