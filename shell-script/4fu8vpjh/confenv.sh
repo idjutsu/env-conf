6ddfcf733d17e70eb606c60a9b467e8b87c2a271
IDShogun <me.creampuffs@gmail.com>
2024-03-16 16:37:46 +0900

    Add shell-script

---
+++ shell-script/4fu8vpjh/confenv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf *
+ls -a | grep -v -E ".|..|.env" | xargs rm -rf
+
+source .env
+
+git clone https://github.com/id-shogun/envconf.git
+
+cp ./envconf/dockerfile/$DOCKER_FILE ./Dockerfile
+cp ./envconf/docker-compose/$DOCKER_COMPOSE.yaml ./compose.yaml
+cp -r ./envconf/nvim/$NVIM_CONF/ ./nvim/
