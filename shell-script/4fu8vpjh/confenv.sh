9961538d4b93ee88ec273be0e1f2656e6d980c2c
IDShogun <me.creampuffs@gmail.com>
2024-03-23 01:38:26 +0900

    Add .tmux.conf

--- shell-script/4fu8vpjh/confenv.sh
+++ shell-script/4fu8vpjh/confenv.sh
@@ -10,3 +10,4 @@ git clone https://github.com/id-shogun/envconf.git
 cp ./envconf/dockerfile/$DOCKER_FILE ./Dockerfile
 cp ./envconf/docker-compose/$DOCKER_COMPOSE.yaml ./compose.yaml
 cp -r ./envconf/nvim/structures/$NVIM_CONF/ ./nvim/
+cp ./envconf/tmux/$TMUX_CONF ./tmux.conf
--- shell-script/4fu8vpjh/setConfOnHome.sh
+++ shell-script/4fu8vpjh/setConfOnHome.sh
@@ -1,8 +1,10 @@
 rm -rf ~/bin/
 rm -rf ~/.config/nvim/
 rm -rf ~/envconf/
+rm ~/.tmux.conf
 
 git clone https://github.com/id-shogun/envconf.git
 
 cp -r ~/envconf/nvim/structures/njojcntx/ ~/.config/nvim/
 cp -r ~/envconf/shell-script/4fu8vpjh/ ~/bin/
+cp ~/envconf/tmux/yhk8tni8 ~/.tmux.conf
