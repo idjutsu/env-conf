717672d7d29522d42db60ba00813f0447fefb34a
IDShogun <me.creampuffs@gmail.com>
2024-03-23 02:02:23 +0900

    Update shell-script/4fu8vpjh

--- shell-script/4fu8vpjh/setConfOnHome.sh
+++ shell-script/4fu8vpjh/setConfOnHome.sh
@@ -7,4 +7,4 @@ git clone https://github.com/id-shogun/envconf.git
 
 cp -r ~/envconf/nvim/structures/njojcntx/ ~/.config/nvim/
 cp -r ~/envconf/shell-script/4fu8vpjh/ ~/bin/
-cp ~/envconf/tmux/yhk8tni8 ~/.tmux.conf
+cp ~/envconf/.tmux.conf ~/.tmux.conf
