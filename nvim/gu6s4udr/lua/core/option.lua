8126c74fdd0e3a2f45a4f62a5bd26a75488649c3
one-man-shogun <me.creampuffs@gmail.com>
2024-09-04 22:49:56 +0900

    Update

--- nvim/gu6s4udr/lua/core/option.lua
+++ nvim/gu6s4udr/lua/core/option.lua
@@ -6,6 +6,8 @@ vim.opt.ai = true
 --vim.opt.background = "dark"
 vim.opt.bri = true
 --vim.opt.clipboard:append("unnamedplus")
+vim.opt.cuc = true
+vim.opt.cul = true
 vim.opt.et = true
 vim.opt.nu = true
 vim.opt.sw = 4
